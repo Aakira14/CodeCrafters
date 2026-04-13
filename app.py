2aae4c2bc6d4267b76b3716eefa9875998eba0e6
RahulPagare20 <amolrahul10@gmail.com>
2026-04-13 08:41:44 +0000

    first commit from server

---
+++ app.py
@@ -0,0 +1,41 @@
+###############################################
+# Backend source code for team: CodeCrafters  #
+# By, Rahul. Pagare (backend dev)             #
+###############################################
+
+
+# Flask modules
+from flask import Flask
+from flask import (
+    Flask,
+    make_response,
+    redirect,
+    render_template,
+    request,
+    send_file,
+    url_for,
+)
+
+#from flask_cors import CORS, cross_origin
+from flask_httpauth import HTTPBasicAuth
+from flask_wtf import CSRFProtect
+
+app = Flask(__name__)
+app.secret_key = "%YTHT34%^$#@CodeCrafters$T#$RT%@%F$$#WTRv!"
+
+csrf = CSRFProtect(app)
+
+
+
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+
+
+@app.route('/inner/stylesheet/style_index.html.css')
+def style_index():
+    global cwd, project_dir
+    return send_file(
+        f'/home/RahulP/CodeCrafters/stylesheets/style_index.html.css')
